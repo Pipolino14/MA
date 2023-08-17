f4864878f6dcf0055e4a1c325eefd127b35ffadd
Felipe <mikeschenberger@gmail.com>
2023-08-17 11:02:33 +0200

    Changed Coding Assets to Assets. Background is drawn (ONe Color)

--- Code/Main.py
+++ Code/Main.py
@@ -1,15 +1,34 @@
 import pygame
+import os
 
 WIDTH, HEIGHT = 1600, 900
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("AI Hunters")
-def main():
 
+#Color Library
+BG_1color = (255, 255, 255)
+
+#Frames Per Second Limiter
+FPS = 60
+
+#Assets loading
+
+
+def draw_window():
+    WIN.fill(BG_1color)
+    pygame.display.update()
+
+def main():
+    clock = pygame.time.Clock()
     run = True
     while run:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        
+        draw_window()
+
     pygame.quit()
 
 if __name__ == "__main__":
