d676a5643fa841a4b7a8e72a10c2b247a186f0eb
Felipe <mikeschenberger@gmail.com>
2023-08-23 21:05:01 +0200

    Added the hitbox of pictures and trying to move them

--- Code/Main.py
+++ Code/Main.py
@@ -10,6 +10,7 @@ BG_1color = (255, 255, 255)
 
 #Frames Per Second Limiter
 FPS = 60
+VEL = 5
 
 #Picture height & length
 ANIMAL_LENGTH = 500
@@ -17,16 +18,19 @@ ANIMAL_HEIGHT = (ANIMAL_LENGTH/16)*9
 
 #Assets loading
 HUNTER_IMAGE = pygame.image.load(os.path.join('Assets', 'fogs.png'))
-HUNTER = pygame.transform.scale(HUNTER_IMAGE, (ANIMAL_LENGTH, ANIMAL_HEIGHT))
+HUNTER_IMAGE_RESIZED = pygame.transform.scale(HUNTER_IMAGE, (ANIMAL_LENGTH, ANIMAL_HEIGHT))
 
 
 
-def draw_window():
+def draw_window(hunter):
     WIN.fill(BG_1color)
-    WIN.blit(HUNTER, (0, 0))
+    WIN.blit(HUNTER_IMAGE_RESIZED, (hunter.x, hunter.y))
     pygame.display.update()
 
 def main():
+    #drawing rectangles for movement of pictures
+    hunter = pygame.Rect(100, 300, ANIMAL_LENGTH, ANIMAL_HEIGHT) 
+
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -35,7 +39,12 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
         
-        draw_window()
+        #WASD-Key movement (Have to figure out a way to make it an FOV and turn it with WASD)
+        keys_pressed = pygame.key.get_pressed()
+        if keys_pressed[pygame.K_d]:
+            hunter.x += VEL
+        
+        draw_window(hunter)
 
     pygame.quit()
 
